6d1af69a621c57c749f68a3ec64a7ba7b162a862
黄煜 <hy10051314@163.com>
2021-10-09 11:37:55 +0800

    个人信息

--- src/view/home/personal.vue
+++ src/view/home/personal.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="personal">
     <div class="personal-table">
-<<<<<<< HEAD
       <el-descriptions direction="vertical" :column="3" border>
         <el-descriptions-item label="用户名">{{
           username
@@ -73,48 +72,6 @@
         </div>
       </el-dialog>
        
-=======
-        <el-descriptions class="margin-top" title="带边框列表" :column="3" size="" border>
-    <template slot="extra">
-      <el-button type="primary" size="small">操作</el-button>
-    </template>
-    <el-descriptions-item>
-      <template slot="label">
-        <i class="el-icon-user"></i>
-        用户名
-      </template>
-      kooriookami
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template slot="label">
-        <i class="el-icon-mobile-phone"></i>
-        手机号
-      </template>
-      18100000000
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template slot="label">
-        <i class="el-icon-location-outline"></i>
-        居住地
-      </template>
-      苏州市
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template slot="label">
-        <i class="el-icon-tickets"></i>
-        备注
-      </template>
-      <el-tag size="small">学校</el-tag>
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template slot="label">
-        <i class="el-icon-office-building"></i>
-        联系地址
-      </template>
-      江苏省苏州市吴中区吴中大道 1188 号
-    </el-descriptions-item>
-  </el-descriptions>
->>>>>>> 9c1e58ef1ddd494d1802c5455b7ffaaa7f0266ee
     </div>
   </div>
 </template>
@@ -192,33 +149,33 @@ export default {
         this.form[i] = ''
       }
     },
-    edit () {
-      // $ajax请求
-      const url = ''
-      // const {data:res}=await
-      const data = {
-          originalpass: this.form.originalpass,
-          password: this.form.pass,   
-      }
-      this.$api(
-        url,
-        data
-      )
-        .then(res => {
-          if (res.code === '101') {
-            this.$message({
-              type: 'success',
-              message: '修改成功!'
-            })
-            this.dialogFormVisible = false
-          } else if (res.code === '99') {
-            this.$message.error('用户名重复!')
-          }
-        })
-        .catch(() => {
-          this.$message.error('网络异常') // element失败提示框上部
-        })
-    },
+    // edit () {
+    //   // $ajax请求
+    //   const url = ''
+     
+    //   const data = {
+    //       originalpass: this.form.originalpass,
+    //       password: this.form.pass,   
+    //   }
+    //   this.$api(
+    //     url,
+    //     data
+    //   )
+    //     .then(res => {
+    //       if (res.code === '101') {
+    //         this.$message({
+    //           type: 'success',
+    //           message: '修改成功!'
+    //         })
+    //         this.dialogFormVisible = false
+    //       } else if (res.code === '99') {
+    //         this.$message.error('用户名重复!')
+    //       }
+    //     })
+    //     .catch(() => {
+    //       this.$message.error('网络异常') // element失败提示框上部
+    //     })
+    // },
   }
 }
 </script>
